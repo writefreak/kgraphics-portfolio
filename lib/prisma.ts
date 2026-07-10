d092fedf42a151a3e1c6a3f6bfaff712312e1773
Writefreak <iscribbledit@gmail.com>
2026-07-11 00:07:39 +0100

    update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,19 @@
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "./generated/prisma/client";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "./generated/prisma";
 
-console.log("DATABASE_URL:", JSON.stringify(process.env.DATABASE_URL));
+// console.log("DATABASE_URL:", JSON.stringify(process.env.DATABASE_URL));
+// // const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
 // const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../prisma/generated/client";
 
 const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
 
